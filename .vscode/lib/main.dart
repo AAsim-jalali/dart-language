5537cb34d1c3e79a9fb99bacce8c050c44bc07a2
Asim Jilale <asimjilale@192.168.10.27>
2023-02-21 12:25:20 +0500

    comment paragrap shift to new line multipil comment syntax and compile

--- .vscode/lib/main.dart
+++ .vscode/lib/main.dart
@@ -1,5 +1,15 @@
 void main() {
+  // this is the method of single comment
+  // this my first parogram
   print('hello world');
+  // this is my family name in aseding order
+  // this is the metod of multiple comment
+  /* 
+  1. this my first parogarming
+  2. this my family name in assecding order
+  3. i m going to be a good parograming
+  4. today i seek the comment  
+  */
   print('this is muhammad asim');
   print('lets start dart.');
   print('father name');
@@ -11,6 +21,7 @@ void main() {
   print('*         *');
   print('*         *');
   print('**********');
+  // this is my assihgment
   print('assigment');
   print('gram to create  square shape with steric(*)');
   print('Write dart program rectangle shape with steric(*)');
@@ -19,6 +30,7 @@ void main() {
   print('Write art program circle shape with  steric(*)');
   print('Write art program Heart shape with steric (*)');
   print(
+      //this is the first part of my assigment
       'Write art program to print full pyramid using *Assignment (Post screenshot in the group link is given above)');
   print('*****************');
   print('*  r             *');
@@ -38,4 +50,34 @@ void main() {
   print('     *       *');
   print('     *       *');
   print('     ********');
+  print('Write art program circle shape with  steric(*)');
+  print('         * *');
+  print('       *       *');
+  print('     *          *');
+  print('    *            *');
+  print('    *            *');
+  print('     *          *');
+  print('       *       *');
+  print('         *   *');
+  print('          * *');
+  // today i learn the syntax and copmpilishion
+  /*
+  1. what is syntax and compilishion
+  2. i m gojng to be print it
+   */
+  print('syntx is called tareqaa kar doing mthod');
+  print('complie is called the show it');
+  // shit to second line by \n
+  print('aasim jalali\n is my best friend\n he is my class fellow');
+  print('my next vadio number is 9');
+  /*Assignment (Postcode screenshot in the group link is given above),
+Complete these task with \n in single line of code as i did in tutorial
+Print your all family name in ascending order 
+Write a dart program square shape with steric(*) 
+Write a dart program rectangle shape with steric(*)
+Write a dart program triangle shapes with  steric(*)
+Write a dart program pentagon shape with steric(*)
+Write a dart program circle shape with  steric(*)
+Write a dart program to print full pyramid using * */
+  print('       *\n      ***\n     ******\n     *******');
 }
